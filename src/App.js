7621d8346d67f935d1609473877f2d561c1547a9
kimseongtae1 <tjdxo7745@gmail.com>
2023-09-05 12:52:30 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,42 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+function List({data, setData}) {
+
+  const remove =(id)=>{
+    axios.delete(`${process.env.REACT_APP_SERVER}/abc/${id}`)
+    .then(res=>{
+      setData(res.data);
+    })
+  }
+
+return(
+  <>
+    {
+      data.map(obj=>(
+        <li key={obj.id}>
+          {obj.msg}
+          <button onClick={()=>{remove(obj.id)}}>삭제</button>
+        </li>
+      ))
+    }
+  </>
+)
+ 
+}
+
+function Write({setData}) {
+  const insert = (e)=>{
+    e.preventDefault();
+
+    let msg = e.target.msg.value;
+
+    axios.post(`${process.env.REACT_APP_SERVER}/insert`,{msg})
+    .then(res=>{
+      setData(res.data);
+    })
+    
+  }
+
+  return(
+
+    <div>
+      <form onSubmit={insert}>
+        <input type='text' name='msg'/>
+        <input type='submit' value='저장'/>
+      </form>
+    </div>
+  )
+
+}
 
 function App() {
 
-  axios.get('https://port-0-jsonserver-6w1j2alm48baij.sel5.cloudtype.app/')
-  .then(res=>{
-    console.log(res)
-  });
+  const [data,setData] = useState([]);
 
-  // axios.post('http://localhost:3030/insert',{id:1000,name:'신규데이터'})
-  // .then(res => {
-  //   console.log(res);
-  // })
-
-
-
-
-
+  const getData = () => {
+    axios.get(`${process.env.REACT_APP_SERVER}/abc`)
+    .then(res=>{
+      setData(res.data);
+    });
+  }
+  useEffect(() => {
+    getData();
+  }, [data])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>한줄댓글(7)</h2>
+      <Write setData={setData}/>
+      
+      <ul>
+       <List data={data} setData={setData}/>
+      </ul>
     </div>
   );
+
 }
 
 export default App;
